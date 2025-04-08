9d73d5458575edd576a8704130cae4f2cbbcdf40
sayyidazizii <sayyidsyafiq234@gmail.com>
2025-04-08 15:06:35 +0700

    feat(Middleware): create middleware auth user

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $adminRole = Role::create(['name' => 'admin']);
+        $userRole = Role::create(['name' => 'user']);
+        $supervisorRole = Role::create(['name' => 'supervisor']);
+
+        $createDepositPermission = Permission::create(['name' => 'create Deposit']);
+        $editDepositPermission = Permission::create(['name' => 'edit Deposit']);
+        $deleteDepositPermission = Permission::create(['name' => 'delete Deposit']);
+
+        $adminRole->givePermissionTo([$createDepositPermission, $editDepositPermission, $deleteDepositPermission]);
+        $userRole->givePermissionTo($createDepositPermission);
+        $supervisorRole->givePermissionTo([$createDepositPermission, $editDepositPermission]);
+
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+        $admin->assignRole('admin');
+
+        $user = User::create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+        $user->assignRole('user');
+
+        $supervisor = User::create([
+            'name' => 'supervisor',
+            'email' => 'supervisor@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+        $supervisor->assignRole('supervisor');
+
+    }
+}
