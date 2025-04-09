d0e61052ceb53780dc4b82c123a7050f39dc9091
sayyidazizii <sayyidsyafiq234@gmail.com>
2025-04-09 11:49:07 +0700

    add git ignore

---
+++ config/duitku.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'merchant_code' => env('DUITKU_MERCHANT_CODE', 'DS12345'),
+    'api_key' => env('DUITKU_API_KEY', 'API_KEY_DARI_DUITKU'),
+    'sandbox' => env('DUITKU_SANDBOX', true),
+];
