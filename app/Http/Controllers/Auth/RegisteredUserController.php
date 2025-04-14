341e6892b80e9ea586df5ef37cc76937e7f6dd0d
sayyidazizii <sayyidsyafiq234@gmail.com>
2025-04-15 00:25:35 +0700

    update register user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,15 +31,21 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'phone' => $request->phone,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        $user->wallet()->create([
+            'balance' => 0,
+        ]);
+        $user->assignRole('user');
 
         event(new Registered($user));
 
